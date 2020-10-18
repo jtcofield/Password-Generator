2f155783b869d9dc60b3eb8116354896f288347f
jtcofield <jtcofield@gmail.com>
2020-10-17 20:03:39 -0400

    fianl application

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password-Generator
+
+Basic password app where the user can choose how long the password is and what type of characters are included (upper or lowercase, special characters, numbers).
--- script.js
+++ script.js
@@ -10,16 +10,69 @@ function writePassword() {
 
 }
 
-function generatePassword(){
-  var passwordLenght = prompt("How many characters do you want your password to be?");
-  var upperCase = confirm ("Include uppercase characters?")
-  var lowerCase = confirm("Include lowercase characters?")
-  var specialCharacters = confirm("Include special characters?")
-  var numericCharacters = confirm("Include numbers?")
+function generatePassword(numOfCharacters){
+  
+  var text = "";
+  var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+  var char_list_two = "abcdefghijklmnopqrstuvwxyz"
+  var char_list_three = "1234567890"
+  var char_list_four = "`~!@#$%^&*()-_=+\|]}[{';:/?.>"
+  var passwordLength = prompt("How many characters do you want your password to be?");
+  var upperCase = confirm ("Include uppercase characters?");
+  var lowerCase = confirm("Include lowercase characters?");
+  var specialCharacters = confirm("Include special characters?");
+  var numericCharacters = confirm("Include numbers?");
+  
+  parseInt(passwordLength);
+  console.log(passwordLength);
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(specialCharacters);
+  console.log(numericCharacters);
+  console.log(Number(upperCase + lowerCase + specialCharacters + numericCharacters));
+
+  if (passwordLength > 128) {
+    alert("Password must be less than 128 characters");
+    location.reload()
+  } 
+  if (passwordLength < 5){
+    alert("Password must be greater than 5 characters")
+    location.reload()
+  }
+
+  // if (upperCase === true) {
+  //   for (i=0; i < (passwordLength/4); i++){
+  //     text += char_list.charAt(Math.floor(Math.random() * char_list.length) +1)
+  //   }}
   
   
+  // if (lowerCase === true){
+  //   for (i=0; i < (passwordLength/4); i++){
+  //   text += char_list_two.charAt(Math.floor(Math.random() * char_list_two.length) +1)}
+  // }
+
+  // if (numericCharacters === true){
+  //   for (i=0; i < (passwordLength/4); i++){
+  //   text += char_list_three.charAt(Math.floor(Math.random() * char_list_three.length) +1)}
+  // }
+
+  // if (specialCharacters === true){
+  //   for (i=0; i < (passwordLength/4); i++){
+  //   text += char_list_four.charAt(Math.floor(Math.random() * char_list_four.length) +1)}
+  // }
+
+  for (i=0; i < passwordLength/(Number(upperCase + lowerCase + numericCharacters + specialCharacters)); i++){
+    if (upperCase === true) {text += char_list.charAt(Math.floor(Math.random() * char_list.length + 1))};
+    if (lowerCase === true) {text += char_list_two.charAt(Math.floor(Math.random() * char_list_two.length + 1))};
+    if (numericCharacters === true) {text += char_list_three.charAt(Math.floor(Math.random() * char_list_three.length + 1))};
+    if (specialCharacters === true) {text += char_list_four.charAt(Math.floor(Math.random() * char_list_four.length + 1))};
+  }
+  
+  console.log(text)
+  return text;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
